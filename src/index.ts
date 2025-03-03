607fa50ee19664c44d88238137a9126f94cb59db
Yemlahi Soufiane <s.yemlahi@dialy.net>
2025-03-03 22:49:56 +0000

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express"
+const app = express()
+const port = 3000
+
+app.get('/home', (req, res) => {
+    console.log("kjnsjdn");
+    
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
